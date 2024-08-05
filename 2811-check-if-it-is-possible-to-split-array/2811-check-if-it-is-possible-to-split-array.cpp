6414627596af54a44bcd1cc2c8536bf8cafbee34
rami affes <76079346+ramiaffes@users.noreply.github.com>
2024-08-05 20:26:31 +0200

    Time: 928 ms (5.25%), Space: 431.4 MB (5.25%) - LeetHub

---
+++ 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    map<pair<int,int>,bool>dp;
+    map<pair<int,int>,bool>vis;
+    void func(vector<int> nums,int i,int j,int m,int som){
+        vis[make_pair(i,j)]=1;
+        if((i==j)){dp[make_pair(i,j)]=1;return;}
+        if(som<m){dp[make_pair(i,j)]=0;return;}
+        if(vis[make_pair(i+1,j)]==0)
+        func(nums,i+1,j,m,som-nums[i]);
+        if(vis[make_pair(i,j-1)]==0)
+        func(nums,i,j-1,m,som-nums[j]);
+        dp[make_pair(i,j)]=(((dp[make_pair(i+1,j)])or(dp[make_pair(i,j-1)]))or((j-i+1)<=2));
+        
+        
+    }
+    bool canSplitArray(vector<int>& nums, int m) {
+        if(nums.size()<=2)return true;
+        dp.clear();
+        vis.clear();
+        int som=0;
+        for(int i=0;i<nums.size();i++)som+=nums[i];
+        func(nums,0,nums.size()-1,m,som);
+        return dp[make_pair(0,nums.size()-1)];
+        }
+};
