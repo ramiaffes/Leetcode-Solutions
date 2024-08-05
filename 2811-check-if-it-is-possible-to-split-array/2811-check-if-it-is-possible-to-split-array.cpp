fccbe1741b417bf427b4c8c3856a35eb91401812
rami affes <76079346+ramiaffes@users.noreply.github.com>
2024-08-05 20:28:28 +0200

    Time: 836 ms (5.25%), Space: 429.1 MB (5.25%) - LeetHub

--- 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.cpp
+++ 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.cpp
@@ -3,14 +3,16 @@ public:
     map<pair<int,int>,bool>dp;
     map<pair<int,int>,bool>vis;
     void func(vector<int> nums,int i,int j,int m,int som){
+        
         vis[make_pair(i,j)]=1;
         if((i==j)){dp[make_pair(i,j)]=1;return;}
         if(som<m){dp[make_pair(i,j)]=0;return;}
+        if(((j-i+1)<=2)){dp[make_pair(i,j)]=1;return;}
         if(vis[make_pair(i+1,j)]==0)
         func(nums,i+1,j,m,som-nums[i]);
         if(vis[make_pair(i,j-1)]==0)
         func(nums,i,j-1,m,som-nums[j]);
-        dp[make_pair(i,j)]=(((dp[make_pair(i+1,j)])or(dp[make_pair(i,j-1)]))or((j-i+1)<=2));
+        dp[make_pair(i,j)]=((dp[make_pair(i+1,j)])or(dp[make_pair(i,j-1)]));
         
         
     }
