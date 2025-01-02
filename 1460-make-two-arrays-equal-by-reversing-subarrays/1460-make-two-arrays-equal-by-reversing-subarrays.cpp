12fb629bcf4a64d5fc04a1666219c06feba4417c
rami affes <76079346+ramiaffes@users.noreply.github.com>
2025-01-02 10:27:47 +0100

    Time: 12 ms (6.93%), Space: 20.8 MB (8.91%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        sort(target.begin(),target.end());
-        sort(arr.begin(),arr.end());
-        return (target==arr);
+        map<int,int>hashing;
+         map<int,int>hashing2;
+        for(int i=0;i<target.size();i++){
+            hashing[target[i]]++;
+        }
+        for(int i=0;i<arr.size();i++){
+            hashing2[arr[i]]++;
+        }
+    for(auto v:hashing){
+        if(hashing[v.first]!=hashing2[v.first])return false;
+    }
+    return true;
     }
 };
