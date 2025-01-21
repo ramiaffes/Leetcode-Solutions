ee53db8dbae5d9520386bd007a37b56554ff0acf
rami affes <76079346+ramiaffes@users.noreply.github.com>
2025-01-22 00:05:38 +0100

    Time: 124 ms (8%), Space: 91.1 MB (6.72%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-        map<int,int>hashing;
+        map<long long int,int>hashing;
+        long long int k1=k;
         for(int i=0;i<arr.size();i++){
-            hashing[arr[i]%k]++;
+         hashing[(arr[i]+1000000000*k1)%k1]++;
         }
         if(hashing[0]%2==1)return false;
         for(int i=1;i<k;i++){
