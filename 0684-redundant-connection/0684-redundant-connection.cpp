4ff2b8f30136fcb8ab1a16291a52a71f866e19d6
rami affes <76079346+ramiaffes@users.noreply.github.com>
2025-01-29 15:47:10 +0100

    Time: 19 ms (8.91%), Space: 17.9 MB (9.44%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
    map<int,vector<int>>adj;
    map<int,bool>vis;
+   int val=0;
    map<pair<int,int>,bool>vis2;
    bool test=false;
+   vector<pair<int,int>>res;
    void dfs(int u){
     if(test==true)return;
     
-	if(vis[u]==1){test=true;return;}
+	if(vis[u]==1){val=u;test=true;return;}
 	vis[u]=1;
     
     for(auto v:adj[u]){
-    if(vis2[make_pair(u,v)]!=1){vis2[make_pair(v,u)]=1;vis2[make_pair(u,v)]=1;
+    if(vis2[make_pair(u,v)]!=1){vis2[make_pair(v,u)]=1;vis2[make_pair(u,v)]=1;res.push_back(make_pair(u,v));
 	dfs(v);}}
 	}
    
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         test=false;
+         val=0;
+        res.clear();
         vis2.clear();
         vis.clear();
         adj.clear();
@@ -25,24 +29,26 @@ public:
             adj[edges[i][1]].push_back(edges[i][0]);
 
         }
-        vector<int>res;
+        
         dfs(1);
-        
-        for(auto v:edges){
-           
-                if((vis[v[0]]==1)and(vis[v[1]]==1)and(adj[v[0]].size()>=2)and(adj[v[1]].size()>=2)){
-                    if(!res.empty())res.clear();
-                    res.push_back(v[0]);
-                    res.push_back(v[1]);
-                   
-                   
+        map<pair<int,int>,bool>vis3;
+        int val3=val;
+        for(int i=res.size()-1;i>=0;i--){
+            if(res[i].second==val){
+                vis3[res[i]]=1;
+                val=res[i].first;
+                if(val==val3)break;
+            }
 
-                }
-            
+        }vector<int>res2;
+        for(auto v:edges){
+            if(vis3[make_pair(v[0],v[1])])res2= v;
+            if(vis3[make_pair(v[1],v[0])])res2= v;
         }
-        for(auto v:vis)cout<<v.first<<endl;
         
-        return res;
+        
+        
+        return res2;
         
             }
 };
